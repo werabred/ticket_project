178ed32a5384da1d4b7ce716c0acaeb3b325f480
werabred <boot718@gmail.com>
2023-05-07 03:14:53 -0400

    e

--- manage.py
+++ manage.py
@@ -52,7 +52,7 @@ def modify():
          ticketid = request.form['ticketid']
          with sql.connect(host="localhost", user="flask1", password="ubuntu", database="tickets_db") as con:
             cur = con.cursor()
-            cmd = "update tickets set Notes = '{0}' where ticketid = '{1}';".format(notes,ticketid)
+            cmd = "update tickets set Notes = '{0}' where ticketid = {1}".format(notes,ticketid)
             cur.execute(cmd)
             
             con.commit()
